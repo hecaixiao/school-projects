9e52a1e0b938b96c760b46f036fd309965c17763
Rosemary Sun <146057261+hecaixiao@users.noreply.github.com>
2023-11-26 19:45:16 +0800

    Add files via upload

---
+++ 用Matlab合成模拟拨号音频并解码/dtmf_decoder.m
@@ -0,0 +1,73 @@
+close all   
+format long g
+              %Muhammed Ali LALE 20011045 Sinyaller ve Sistemler Grup 2            
+key =  ['1','2','3'
+        '4','5','6'
+        '7','8','9'
+        '*','0','#'];
+[tel,fs]=audioread('学号音频文件.wav');  %decode edilmek istenen ses kaydı
+
+n = 10; %tuş sayısı
+len=length(tel); %sinyalin uzunluğu
+d = floor(len/n); %tuş sesi uzunluğu
+T = 1/fs; %periyot
+t = (0:d-1)*T; %time vector
+
+num=1:n; %tuşların tutulduğu vector
+tell = zeros(n,d);  %her bir tuşun tutulacağı matris
+for k=1:n
+    tell(k,1:d) = tel((k-1)*d+1:k*d);
+    Y = abs(fft(tell(k,:)));
+    P = Y/d;
+    P1 = P(1:d/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = fs*(0:(d/2))/d;
+   
+    peak=P1(1); %tepe noktası bulma
+for i=2:d/2+1
+    if (peak<P1(i))
+        peak = P1(i);
+        d1=i;
+    end
+end
+    peak=P1(1); %2. tepe noktası bulma
+for i=2:d/2+1
+    if (i~=d1 && peak<P1(i))
+        peak = P1(i);
+        d2=i;
+    end
+end
+%tepe noktalarındaki frekans değerleri
+    F1=f(d1);   
+    F2=f(d2);   
+%bulunan frekans değerlerinin hangisinin Low/High Frequency olduğunu belirleme    
+    if (F1<F2)
+        FL=F1;
+        FH=F2;
+    else
+        FL=F2;
+        FH=F1;
+    end
+%Frekans değerlerine göre tuşların bulunduğu satır ve sütunları belirleme
+    if(FL>690 && FL<705)
+        L=1;
+    elseif(FL>760 && FL<780)
+            L=2;
+    elseif(FL>840 && FL<860)
+            L=3;
+    elseif(FL>930)
+            L=4;
+    end
+
+ if(FH>1200 && FH<1220)
+      H=1;
+ elseif(FH>1330 && FH<1350)
+      H=2;
+ elseif(FH>1470)
+      H=3;
+ end
+ num(k)=key(L,H);
+
+end
+
+fprintf('\n\tTotal Number is: %s\n',num);
